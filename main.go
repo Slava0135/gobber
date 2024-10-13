68a801000b5ab304a719631835ee974c69aa2973
slava0135 <super.novalskiy_0135@inbox.ru>
2024-10-13 13:22:18 +0300

    uncomment main

--- main.go
+++ main.go
@@ -3,25 +3,25 @@ package main
 import "slava0135/gobber/constraints"
 
 func main() {
-	// constraints.IntegerOperations()
-	// constraints.FloatOperations()
-	// constraints.MixedOperations()
-	// constraints.NestedConditions()
-	// constraints.BitwiseOperations()
-	// constraints.AdvancedBitwise()
-	// constraints.CombinedBitwise()
-	// constraints.NestedBitwise()
+	constraints.IntegerOperations()
+	constraints.FloatOperations()
+	constraints.MixedOperations()
+	constraints.NestedConditions()
+	constraints.BitwiseOperations()
+	constraints.AdvancedBitwise()
+	constraints.CombinedBitwise()
+	constraints.NestedBitwise()
 
-	// constraints.BasicComplexOperations()
-	// constraints.ComplexMagnitude()
-	// constraints.ComplexComparison()
-	// constraints.ComplexOperations()
-	// constraints.NestedComplexOperations()
+	constraints.BasicComplexOperations()
+	constraints.ComplexMagnitude()
+	constraints.ComplexComparison()
+	constraints.ComplexOperations()
+	constraints.NestedComplexOperations()
 
-	// constraints.CompareElement()
-	// constraints.CompareAge()
+	constraints.CompareElement()
+	constraints.CompareAge()
 
-	// constraints.PushPopIncrementality()
+	constraints.PushPopIncrementality()
 
 	constraints.CompareAndIncrement()
 }
