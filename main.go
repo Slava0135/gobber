1956da4bc7e07aa732b82f67abc3850c0434ba2e
slava0135 <super.novalskiy_0135@inbox.ru>
2024-10-21 12:00:27 +0300

    add flags

--- main.go
+++ main.go
@@ -1,34 +1,58 @@
 package main
 
 import (
+	"flag"
+
 	"slava0135/gobber/constraints"
 	"slava0135/gobber/subtypes"
 )
 
 func main() {
-	constraints.IntegerOperations()
-	constraints.FloatOperations()
-	constraints.MixedOperations()
-	constraints.NestedConditions()
-	constraints.BitwiseOperations()
-	constraints.AdvancedBitwise()
-	constraints.CombinedBitwise()
-	constraints.NestedBitwise()
+	runNumbers := flag.Bool("numbers", false, "solve numbers constraints")
+	runComplex := flag.Bool("complex", false, "solve complex constraints")
+	runArrays := flag.Bool("arrays", false, "solve arrays constraints")
+	runPushPop := flag.Bool("pushpop", false, "solve constraints with push-pop incrementality")
+	runSoft := flag.Bool("soft", false, "solve soft constraints")
+	runSubtypes := flag.Bool("subtypes", false, "subtyping encoding")
 
-	constraints.BasicComplexOperations()
-	constraints.ComplexMagnitude()
-	constraints.ComplexComparison()
-	constraints.ComplexOperations()
-	constraints.NestedComplexOperations()
+	flag.Parse()
 
-	constraints.CompareElement()
-	constraints.CompareAge()
+	if *runNumbers {
+		constraints.IntegerOperations()
+		constraints.FloatOperations()
+		constraints.MixedOperations()
+		constraints.NestedConditions()
+		constraints.BitwiseOperations()
+		constraints.AdvancedBitwise()
+		constraints.CombinedBitwise()
+		constraints.NestedBitwise()
+	}
 
-	constraints.PushPopIncrementality()
+	if *runComplex {
+		constraints.BasicComplexOperations()
+		constraints.ComplexMagnitude()
+		constraints.ComplexComparison()
+		constraints.ComplexOperations()
+		constraints.NestedComplexOperations()
+	}
 
-	constraints.CompareAndIncrement()
+	if *runArrays {
+		constraints.CompareElement()
+		constraints.CompareAge()
+	}
 
-	subtypes.SubclassesExample()
-	subtypes.SubtypesExample()
-	subtypes.NaiveTypeSolver()
+	if *runPushPop {
+		constraints.PushPopIncrementality()
+	}
+
+	if *runSoft {
+		constraints.CompareAndIncrement()
+	}
+
+	if *runSubtypes {
+		subtypes.SubclassesExample()
+		subtypes.SubtypesExample()
+		subtypes.NaiveTypeSolver()
+	}
+
 }
