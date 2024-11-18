966eb036132e83f94639936bcb7aa59ea21e29ee
slava0135 <super.novalskiy_0135@inbox.ru>
2024-11-18 10:37:31 +0300

    move some ssa functions

---
+++ symexec/ssa.go
@@ -0,0 +1,93 @@
+package symexec
+
+import (
+	"fmt"
+	"go/ast"
+	"go/importer"
+	"go/parser"
+	"go/token"
+	"go/types"
+	"strings"
+
+	"golang.org/x/tools/go/ssa"
+	"golang.org/x/tools/go/ssa/ssautil"
+)
+
+func buildPackage(filename string) *ssa.Package {
+	fmt.Printf(":: building SSA graph for file '%s'\n", filename)
+
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, filename, nil, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	files := []*ast.File{f}
+
+	pkg := types.NewPackage("main", "")
+
+	main, _, err := ssautil.BuildPackage(&types.Config{Importer: importer.Default()}, fset, pkg, files, 0)
+	if err != nil {
+		panic(err)
+	}
+	return main
+}
+
+func printBlocks(fn *ssa.Function) {
+	for _, v := range fn.Blocks {
+		fmt.Println(v.String(), "->")
+		for _, v := range v.Instrs {
+			printInstr := func(name string) {
+				if reg, ok := v.(Register); ok {
+					fmt.Printf("  [%10s] %s:%s <-- %s\n", strings.ToUpper(name), reg.Name(), reg.Type(), v.String())
+				} else {
+					fmt.Printf("  [%10s] %s\n", strings.ToUpper(name), v.String())
+				}
+			}
+			switch v.(type) {
+			case *ssa.Alloc:
+				printInstr("alloc")
+			case *ssa.BinOp:
+				printInstr("binop")
+			case *ssa.Call:
+				printInstr("call")
+			case *ssa.Convert:
+				printInstr("convert")
+			case *ssa.Extract:
+				printInstr("extract")
+			case *ssa.Field:
+				printInstr("field")
+			case *ssa.FieldAddr:
+				printInstr("field addr")
+			case *ssa.If:
+				printInstr("if")
+			case *ssa.Index:
+				printInstr("index")
+			case *ssa.IndexAddr:
+				printInstr("index addr")
+			case *ssa.Jump:
+				printInstr("jump")
+			case *ssa.Lookup:
+				printInstr("lookup")
+			case *ssa.MakeMap:
+				printInstr("make map")
+			case *ssa.MakeSlice:
+				printInstr("make slice")
+			case *ssa.MapUpdate:
+				printInstr("map update")
+			case *ssa.Phi:
+				printInstr("phi")
+			case *ssa.Return:
+				printInstr("return")
+			case *ssa.Select:
+				printInstr("select")
+			case *ssa.Store:
+				printInstr("store")
+			case *ssa.UnOp:
+				printInstr("unop")
+			default:
+				panic("unknown instruction")
+			}
+		}
+	}
+}
--- symexec/symexec.go
+++ symexec/symexec.go
@@ -2,10 +2,6 @@ package symexec
 
 import (
 	"fmt"
-	"go/ast"
-	"go/importer"
-	"go/parser"
-	"go/token"
 	"go/types"
 	"os"
 	"runtime/debug"
@@ -13,7 +9,6 @@ import (
 
 	"github.com/aclements/go-z3/z3"
 	"golang.org/x/tools/go/ssa"
-	"golang.org/x/tools/go/ssa/ssautil"
 )
 
 type Register interface {
@@ -34,31 +29,14 @@ func Static() {
 	}
 }
 
-func AnalyzeFile(tc string) map[string]bool {
-	fmt.Printf(":: building SSA graph for file '%s'\n", tc)
-
-	fset := token.NewFileSet()
-	f, err := parser.ParseFile(fset, tc, nil, 0)
-	if err != nil {
-		panic(err)
-	}
-
-	files := []*ast.File{f}
-
-	pkg := types.NewPackage("main", "")
-
-	main, _, err := ssautil.BuildPackage(&types.Config{Importer: importer.Default()}, fset, pkg, files, 0)
-	if err != nil {
-		panic(err)
-	}
-
+func AnalyzeFile(filename string) map[string]bool {
+	main := buildPackage(filename)
 	res := make(map[string]bool, 0)
 	for _, v := range main.Members {
 		if fn, ok := v.(*ssa.Function); ok && fn.Name() != "init" {
 			res[fn.Name()] = staticFunction(fn)
 		}
 	}
-
 	fmt.Println()
 	return res
 }
@@ -78,65 +56,6 @@ func staticFunction(fn *ssa.Function) (ok bool) {
 	fmt.Println("::", "encoding formula")
 	encodeFormula(fn, f)
 	return true
-}
-
-func printBlocks(fn *ssa.Function) {
-	for _, v := range fn.Blocks {
-		fmt.Println(v.String(), "->")
-		for _, v := range v.Instrs {
-			printInstr := func(name string) {
-				if reg, ok := v.(Register); ok {
-					fmt.Printf("  [%10s] %s:%s <-- %s\n", strings.ToUpper(name), reg.Name(), reg.Type(), v.String())
-				} else {
-					fmt.Printf("  [%10s] %s\n", strings.ToUpper(name), v.String())
-				}
-			}
-			switch v.(type) {
-			case *ssa.Alloc:
-				printInstr("alloc")
-			case *ssa.BinOp:
-				printInstr("binop")
-			case *ssa.Call:
-				printInstr("call")
-			case *ssa.Convert:
-				printInstr("convert")
-			case *ssa.Extract:
-				printInstr("extract")
-			case *ssa.Field:
-				printInstr("field")
-			case *ssa.FieldAddr:
-				printInstr("field addr")
-			case *ssa.If:
-				printInstr("if")
-			case *ssa.Index:
-				printInstr("index")
-			case *ssa.IndexAddr:
-				printInstr("index addr")
-			case *ssa.Jump:
-				printInstr("jump")
-			case *ssa.Lookup:
-				printInstr("lookup")
-			case *ssa.MakeMap:
-				printInstr("make map")
-			case *ssa.MakeSlice:
-				printInstr("make slice")
-			case *ssa.MapUpdate:
-				printInstr("map update")
-			case *ssa.Phi:
-				printInstr("phi")
-			case *ssa.Return:
-				printInstr("return")
-			case *ssa.Select:
-				printInstr("select")
-			case *ssa.Store:
-				printInstr("store")
-			case *ssa.UnOp:
-				printInstr("unop")
-			default:
-				panic("unknown instruction")
-			}
-		}
-	}
 }
 
 func makeFormula(fn *ssa.Function) Formula {
